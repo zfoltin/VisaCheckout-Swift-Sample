4123eeda58a998ddd3a86b01649bb1603f70aa51
Zeno Foltin <zeno.foltin@gmail.com>
2017-11-20 21:28:49 +0000

    WIP - awaiting JudoKit 6.2.19 release

---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '9.0'
+
+target 'VisaCheckoutSwiftSample' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for VisaCheckoutSwiftSample
+  pod 'JudoKit', '~> 6.2.19'
+  pod 'VisaCheckout', '~> 5.5.2-9.1'
+end
---
+++ VisaCheckoutSwiftSample/ViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ViewController.swift
+//  VisaCheckoutSwiftSample
+//
+//  Created by Zeno Foltin on 19/11/2017.
+//  Copyright © 2017 Judopay. All rights reserved.
+//
+
+import UIKit
+import VisaCheckoutSDK
+import JudoKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var checkoutButton: VisaCheckoutButton!
+    private let judoKit = JudoKit(token: "<#Your Judopay token#>", secret: "<#Your Judopay secret#>")
+    private let judoId = "<#Your Judopay Id#>"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initVisaCheckout()
+        judoKit.sandboxed(true)
+    }
+
+    private func initVisaCheckout() {
+        /// See the documentation/headers for `PurchaseInfo` for
+        /// various ways to customize the purchase experience.
+        let purchaseInfo = PurchaseInfo(total: 10.99, currency: .gbp)
+        purchaseInfo.reviewAction = .pay
+        checkoutButton.onCheckout(purchaseInfo: purchaseInfo, completion: visaCheckoutResultHandler)
+    }
+
+    private func visaCheckoutResultHandler(result: CheckoutResult) {
+        switch result.statusCode {
+        case .success:
+            print("CallId: \(String(describing: result.callId))")
+            print("Encrypted key: \(String(describing: result.encryptedKey))")
+            print("Payment data: \(String(describing: result.encryptedPaymentData))")
+
+            if let callId = result.callId, let encryptedKey = result.encryptedKey, let encryptedPaymentData = result.encryptedPaymentData {
+                let amount = Amount(decimalNumber: 10.99, currency: .GBP)
+                let vcoResult = VCOResult(callId: callId, encryptedKey: encryptedKey, encryptedPaymentData: encryptedPaymentData)
+                if let reference = Reference(consumerRef: UUID().uuidString),
+                    let transaction = try? Transaction(judoId: judoId, amount: amount, reference: reference)
+                        .vcoResult(vcoResult: vcoResult) {
+                    try? judoKit.completion(transaction, block: judoCompletionBlock)
+                }
+            }
+        case .userCancelled:
+            print("Payment cancelled by the user")
+        default:
+            break
+        }
+    }
+
+    private func judoCompletionBlock(response: Response?, error: JudoError?) {
+        if let response = response, response.items.count > 0, response.items[0].result == .Success {
+            print("Payment successful!")
+        } else {
+            print("Oops. Something went wrong.")
+        }
+    }
+}
